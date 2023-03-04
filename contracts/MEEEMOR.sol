7455505274b5fd80f7febe7789c0d89be1106fd0
kenkun091 <kun.guo91@gmail.com>
2023-03-04 15:32:13 -0700

    add bounty contract

--- contracts/MEEEMOR.sol
+++ contracts/MEEEMOR.sol
@@ -14,25 +14,90 @@ contract MEEME is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
 
     Counters.Counter private _tokenIdCounter;
 
-    struct TokenVote {
-        uint256 tokenId;
-        uint256 votes;
-    }
+    address private poapAddress;
+    uint256 private eventId;
+    uint256 private duration;
+    uint256 private bountyAmount;
+    uint256 private winnerTokenId;
 
-    address public poapAddress;
+    uint256 private startTime;
+    bool private timerStarted;
+    uint256 private endTime;
+
+    address[] private voteWinners;
+    mapping(address => bool) _eligible;
+    uint256 private voteAward;
+
+    event TimerStarted(uint256 startTime);
+    event TimerEnded(uint256 endTime);
+    event BountyAwarded(address winner, uint256 amount);
     
-    mapping (uint256 => TokenVote) private _tokenVotes;
-    mapping (address => mapping (uint256 => bool)) private _voters;
+    mapping(uint256 => uint256) private _tokenVotes;
+    mapping(uint256 => address) private _creators; 
+    mapping(uint256 => address[]) private _voters;
 
     mapping(address => bool) public _voted;
-    mapping(address => mapping(uint256=>bool)) public votes;
+    // mapping(address => mapping(uint256=>bool)) public votes;
 
-    event MintRequested(address indexed account, uint256 tokenId, bool voted);
+    event MemeCreated(address indexed addr, uint256 tokenId);
 
-    constructor(address addr) ERC721("MEEEMOR", "MMR"){
-
-        poapAddress = addr;
+    constructor(address _poapAddr, uint256 _eventId) payable ERC721("MEEEMOR", "MMR"){
+        require(msg.value >=1 ether, "Bounty amount must be greater than 1 EH");
+        eventId = _eventId;
+        poapAddress = _poapAddr;
+        bountyAmount = msg.value;
     }
+
+    function startTimer(uint256 _duration) external {
+        require(!timerStarted, "Timer already started");
+
+        startTime = block.timestamp;
+        endTime = startTime + _duration;
+        timerStarted = true;
+
+        emit TimerStarted(startTime);
+
+    }
+
+    function getRemainingTime() public view returns (uint256) {
+        uint256 currentTime = block.timestamp;
+        if (currentTime >= endTime) {
+            return 0;
+        } else {
+            return endTime - currentTime;
+        }
+    }
+
+    function endBouty() external onlyOwner{
+        require(timerStarted, "Timer not started");
+        require(block.timestamp >= endTime, "Timer not ended");
+
+        timerStarted = false;
+
+        emit TimerEnded(endTime);
+
+        uint256 awardAmount = (bountyAmount*50)/100;
+        uint256 tokenIndex = findLargestIndex();
+
+        address winner = _creators[tokenIndex];
+        voteWinners = _voters[tokenIndex];
+
+        payable(winner).transfer(awardAmount); 
+
+        bountyAmount -= awardAmount;
+        emit BountyAwarded(winner, awardAmount);
+
+        voteAward = bountyAmount*95/100;
+
+        for (uint i = 0; i< voteWinners.length; i++){
+            _eligible[voteWinners[i]] = true;
+        }
+    }
+
+    function getBountyAmount() external view returns (uint256) {
+        return bountyAmount;
+    }
+
 
     function getPoapContract() public virtual view returns (address) {
         return poapAddress;
@@ -44,6 +109,7 @@ contract MEEME is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         _tokenIdCounter.increment();
         _safeMint(address(this), tokenId);
         _setTokenURI(tokenId, uri);
+
     }
 
 
@@ -65,24 +131,51 @@ contract MEEME is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         require(_exists(tokenId), "Token does not exist");
         require(!_voted[msg.sender], "Account has voted");
         
-        _voters[msg.sender][tokenId] = true;
-        _tokenVotes[tokenId].votes += 1;
+        _voted[msg.sender] = true;
+        _tokenVotes[tokenId] += 1;
+
+        _voters[tokenId].push(msg.sender);
+    }
+
+
+    function findLargestIndex() public view returns (uint256) {
+
+        uint256 largestIndex = 0;
+        uint256 largestNumber = _tokenVotes[0];
+
+        uint256 tokenId = _tokenIdCounter.current();
+
+        for (uint256 i = 1; i < tokenId; i++) {
+            if (_tokenVotes[i] > largestNumber) {
+                largestNumber = _tokenVotes[i];
+                largestIndex = i;
+            }
+        }
+
+        return largestIndex;
+    }
+
+    function voterClaim() public {
+        require(_eligible[msg.sender], "Account is not eligible");
+        require(bountyAmount > 0, "No bounty remains");
+        payable(msg.sender).transfer(voteAward);
+        bountyAmount -= voteAward;
     }
     
     function getTokenVotes(uint256 tokenId) public view returns (uint256) {
-        return _tokenVotes[tokenId].votes;
+        return _tokenVotes[tokenId];
     }
-    
-    function getVoterVote(address voter, uint256 tokenId) public view returns (bool) {
-        return _voters[voter][tokenId];
+
+    function getVoteAddress(uint256 tokenId) public view returns (address[] memory){
+        return _voters[tokenId];
     }
 
     // The following functions are overrides required by Solidity.
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
         override(ERC721, ERC721Enumerable)
     {
-        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+        super._beforeTokenTransfer(from, to, tokenId);
     }
 
     function _afterTokenTransfer(address from, address to, uint256 tokenId)
@@ -114,5 +207,3 @@ contract MEEME is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         return super.supportsInterface(interfaceId);
     }
 }
-
-
